1ebc41d8954dc692746b6fda30b16556e3e49032
gehad-magdi <gehadmagdi1990@gmail.com>
2021-05-06 22:30:34 -0700

    edit section three

--- src/components/sectionPartThree.js
+++ src/components/sectionPartThree.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import SecThree from './sectionThreePartOne'
-import SecSlider from './sectionThreePartTwo'
+import React from "react";
+import SecThree from "./sectionThreePartOne";
+import SecSlider from "./sectionThreePartTwo";
+import { Container, Grid } from "@material-ui/core";
 
 class TheardThree extends React.Component {
-    render() {
-        return(
-          <div className='sectionThree'>
-            <div className='secInnerThree'>
-            <SecThree />
-            <SecSlider />
-            </div>
-          </div>
-        )
-    }
+	render() {
+		return (
+			<div className="sectionThree">
+				<Container
+					className="secInnerThree"
+					style={{ paddingTop: 100, paddingBottom: 100 }}
+				>
+					<Grid container spacing={2} alignItems="center">
+						<Grid xs={12} lg={5} style={{ padding: 10 }}>
+							<SecThree />
+						</Grid>
+						<Grid xs={12} lg={7} style={{ padding: 10 }}>
+							<SecSlider />
+						</Grid>
+					</Grid>
+				</Container>
+			</div>
+		);
+	}
 }
 
 export default TheardThree;
